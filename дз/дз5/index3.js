4b82d639e6e61d7696f088cac7e14a256c78c547
Maksimvovkdota2 <128405890+Maksimvovkdota2@users.noreply.github.com>
2023-07-24 15:36:27 +0300

    Delete дз directory

--- дз/дз5/index3.js
+++
@@ -1,6 +0,0 @@
-const a = document.getElementById('name-input');
-const b = document.getElementById('name-output');
-a.addEventListener('input', output);
-function output(event) {
-  b.innerHTML = event.target.value;
-}
